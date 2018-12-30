a540a7a0ff96a47c58529831c63611e726885f98
Dhruv <dhruvchitkara27@gmail.com>
2018-12-30 20:38:26 +0530

    Query filters

--- mongo-project/routes/cities.js
+++ mongo-project/routes/cities.js
@@ -2,20 +2,23 @@ const express = require('express')
 const router = express.Router()
 const City = require('../models/City')
 
-router.get('/',(req,res,next) => {
-  City.find()
-  .then(cities => {
-      res.json({
-        confirmation: 'success',
-        data: cities
-      })
-  })
-  .catch(err => {
-      res.json({
-        confirmation: 'fail',
-        message: err.message
-      })
-  })
+router.get('/', (req, res, next) => {
+	const query = req.query
+
+	City.find(query)
+	.then(cities => {
+		res.json({
+			confirmation: 'success',
+			data: cities
+		})
+	})
+	.catch(err => {
+		res.json({
+			confirmation: 'fail',
+			message: err.message
+		})
+	})
 })
+
 
 module.exports = router
--- mongo-project/server.js
+++ mongo-project/server.js
@@ -1,63 +1,26 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const City = require('./models/City')
-const Country = require('./models/Country')
 
-//connect to our Mongo DB:
-mongoose.connect('mongodb://localhost/world')
+
+// connect to our Mongo DB:
+mongoose.connect('mongodb://localhost/world', { useNewUrlParser: true })
 .then(data => {
-  console.log('Mongo DB connection success!')
+	console.log('Mongo DB connection success!')
 })
 .catch(err => {
-  console.log('Mongo DB connection failed: ' + err.message)
+	console.log('Mongo DB connection failed: ' + err.message)
 })
+
 
 const app = express()
 
 const cities = require('./routes/cities')
-const countries = require('./routes/cities')
+const countries = require('./routes/countries')
 
-app.use('/cities',cities)
+app.use('/cities', cities)
 app.use('/countries', countries)
 
-app.get('/', (req,res,next) => {
-  res.json({
-    confirmation: 'success',
-    data: 'This is the Mongo project!'
-  })
-})
 
-app.get('/cities', (req,res,next) => {
-    City.find(null)
-    .then(cities => {
-      res.json({
-        confirmation: 'success',
-        data: cities
-      })
-    })
-    .catch(err => {
-      res.json({
-        confirmation: 'fail',
-        message: err.message
-      })
-    })
-})
-
-app.get('/countries', (req,res,next) => {
-  Country.find(null)
-  .then(countries => {
-    res.json({
-      confirmation: 'success',
-      data: countries
-    })
-  })
-  .catch(err => {
-    res.json({
-      confirmation: 'fail',
-      message: err.message
-    })
-  })
-})
 
 app.listen(3000)
-console.log('App running http://localhost3000')
+console.log('App running http://localhost:3000')
